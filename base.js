13e7706a6727e60694c1cd197ea7347a50290c9e
いーちゃん(e_chan1007) <echan041007@gmail.com>
2023-08-21 20:00:05 +0900

    chore: upgrade dependencies

--- base.js
+++ base.js
@@ -1,5 +1,6 @@
 /** @type {import("eslint").ESLint.ConfigData} */
 module.exports = {
+  root: true,
   extends: [
     "eslint:recommended",
     "prettier",
--- next.js
+++ next.js
@@ -1,5 +1,5 @@
 /** @type {import("eslint").ESLint.ConfigData} */
 module.exports = {
   root: true,
-  extends: ["./base", "next/core-web-vitals", "./react"]
+  extends: ["next/core-web-vitals", "./react"]
 };
